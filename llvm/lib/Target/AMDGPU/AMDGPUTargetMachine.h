c67fddac648cf48bdd6e181498ffb59cf7cc8d22
Matt Arsenault <Matthew.Arsenault@amd.com>
2025-07-18 17:15:38 +0900

    AMDGPU: Add pass to replace constant materialize with AV pseudos (#149292)
    If we have a v_mov_b32 or v_accvgpr_write_b32 with an inline immediate,
    replace it with a pseudo which writes to the combined AV_* class. This
    relaxes the operand constraints, which will allow the allocator to
    inflate the register class to AV_* to potentially avoid spilling.
    
    The allocator does not know how to replace an instruction to enable
    the change of register class. I originally tried to do this by changing
    all of the places we introduce v_mov_b32 with immediate, but it's along
    tail of niche cases that require manual updating. Plus we can restrict
    this to only run on functions where we know we will be allocating AGPRs.

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.h
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.h
@@ -184,7 +184,9 @@ public:
   void addMachineSSAOptimization(AddMachinePass &) const;
   void addPostRegAlloc(AddMachinePass &) const;
   void addPreEmitPass(AddMachinePass &) const;
+  void addPreEmitRegAlloc(AddMachinePass &) const;
   Error addRegAssignmentOptimized(AddMachinePass &) const;
+  void addPreRegAlloc(AddMachinePass &) const;
   void addOptimizedRegAlloc(AddMachinePass &) const;
 
   /// Check if a pass is enabled given \p Opt option. The option always
