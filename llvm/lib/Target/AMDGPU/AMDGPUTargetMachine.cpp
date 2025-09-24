89c696ae93abe74c407cb7ea95ed326dc23fd387
vikhegde <vikram.hegde@amd.com>
2025-09-24 16:41:35 +0530

    Fixes to be upstreamed - 3

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -2447,10 +2447,10 @@ void AMDGPUCodeGenPassBuilder::addPreEmitPass(AddMachinePass &addPass) const {
   addPass(SIMemoryLegalizerPass());
   addPass(SIInsertWaitcntsPass());
 
-  // TODO: addPass(SIModeRegisterPass());
+  addPass(SIModeRegisterPass());
 
   if (TM.getOptLevel() > CodeGenOptLevel::None) {
-    // TODO: addPass(SIInsertHardClausesPass());
+    addPass(SIInsertHardClausesPass());
   }
 
   addPass(SILateBranchLoweringPass());
