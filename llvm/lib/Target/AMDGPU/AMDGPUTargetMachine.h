f5ebffa0a6763b1a2bc552da09ee62b1a043cb42
vikhegde <vikram.hegde@amd.com>
2025-09-24 16:50:14 +0530

    Fixes to be upstreamed - 13

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.h
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.h
@@ -104,7 +104,8 @@ public:
                              raw_pwrite_stream *DwoOut,
                              CodeGenFileType FileType,
                              const CGPassBuilderOption &Opts, MCContext &Ctx,
-                             PassInstrumentationCallbacks *PIC) override;
+                             PassInstrumentationCallbacks *PIC,
+                             PassBuilder &) override;
 
   void registerMachineRegisterInfoCallback(MachineFunction &MF) const override;
 
