1f98fb1810dbf38c2f5d62b29f146bbb0df97434
vikhegde <vikram.hegde@amd.com>
2025-09-29 14:36:06 +0530

    Refactor code, add a clang option to disable new PM

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -1276,7 +1276,7 @@ GCNTargetMachine::GCNTargetMachine(const Target &T, const Triple &TT,
                                    CodeGenOptLevel OL, bool JIT)
     : AMDGPUTargetMachine(T, TT, CPU, FS, Options, RM, CM, OL) {
     if (getSelectorType(*this) != SelectorType::GlobalISel)
-        setNewPMForBackend(true);
+       setUseNPMForBackend(true);
    }
 
 enum class OOBFlagValue {
