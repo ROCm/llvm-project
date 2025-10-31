5e2be04e9db1d8c519fbe4374aeef738ef95c0e6
Lieberman, Ron <Ron.Lieberman@amd.com>
2025-10-31 07:28:52 -0400

    Revert "Revert " Add builtin/intrinsic global_(load|store)_b128""

--- clang/include/clang/Sema/SemaAMDGPU.h
+++ clang/include/clang/Sema/SemaAMDGPU.h
@@ -30,6 +30,8 @@ public:
 
   bool CheckAMDGCNBuiltinFunctionCall(unsigned BuiltinID, CallExpr *TheCall);
 
+  bool checkScopedMemAccessFunctionCall(CallExpr *TheCall);
+
   bool checkMovDPPFunctionCall(CallExpr *TheCall, unsigned NumArgs,
                                unsigned NumDataArgs);
 
