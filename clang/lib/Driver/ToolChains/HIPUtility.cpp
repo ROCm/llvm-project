c2752978382bdebae6ce0c6b43be7025f624d549
david-salinas <dsalinas@amd.com>
2025-11-06 03:43:17 +0000

    Correct processing of hip_gpubin_handle symbols

--- clang/lib/Driver/ToolChains/HIPUtility.cpp
+++ clang/lib/Driver/ToolChains/HIPUtility.cpp
@@ -241,18 +241,14 @@ private:
 
       bool isUndefined =
           FlagOrErr.get() & llvm::object::SymbolRef::SF_Undefined;
-      bool isHidden =
-          FlagOrErr.get() & llvm::object::SymbolRef::SF_Hidden;
       bool isFatBinSymbol = Name.starts_with(FatBinPrefix);
       bool isGPUBinHandleSymbol = Name.starts_with(GPUBinHandlePrefix);
 
       // Handling for defined symbols
       if (!isUndefined) {
         if (isFatBinSymbol) {
-          DefinedFatBinSymbols.insert(Name.str());
           FatBinSymbols.erase(Name.str());
-        } else if (isGPUBinHandleSymbol && (!isHidden) ) {
-          DefinedGPUBinHandleSymbols.insert(Name.str());
+        } else if (isGPUBinHandleSymbol) {
           GPUBinHandleSymbols.erase(Name.str());
         }
         continue;
