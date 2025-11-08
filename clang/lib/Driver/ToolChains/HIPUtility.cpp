e3089779151a9fd0c85a29aee56e5adc5256bd7b
david-salinas <dsalinas@amd.com>
2025-11-08 15:23:08 +0000

    Revert "Correct processing of hip_gpubin_handle symbols"
    This reverts commit c2752978382bdebae6ce0c6b43be7025f624d549.

--- clang/lib/Driver/ToolChains/HIPUtility.cpp
+++ clang/lib/Driver/ToolChains/HIPUtility.cpp
@@ -241,14 +241,18 @@ private:
 
       bool isUndefined =
           FlagOrErr.get() & llvm::object::SymbolRef::SF_Undefined;
+      bool isHidden =
+          FlagOrErr.get() & llvm::object::SymbolRef::SF_Hidden;
       bool isFatBinSymbol = Name.starts_with(FatBinPrefix);
       bool isGPUBinHandleSymbol = Name.starts_with(GPUBinHandlePrefix);
 
       // Handling for defined symbols
       if (!isUndefined) {
         if (isFatBinSymbol) {
+          DefinedFatBinSymbols.insert(Name.str());
           FatBinSymbols.erase(Name.str());
-        } else if (isGPUBinHandleSymbol) {
+        } else if (isGPUBinHandleSymbol && (!isHidden) ) {
+          DefinedGPUBinHandleSymbols.insert(Name.str());
           GPUBinHandleSymbols.erase(Name.str());
         }
         continue;
