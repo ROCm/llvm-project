5e8d4efa8dfebdda691da20196407a26baf50b04
Ron Lieberman <ron.lieberman@amd.com>
2025-12-13 05:58:49 -0600

    [flang] loop interchange from kruse

--- flang/lib/Parser/openmp-parsers.cpp
+++ flang/lib/Parser/openmp-parsers.cpp
@@ -2466,6 +2466,7 @@ static constexpr DirectiveSet GetLoopDirectives() {
       unsigned(Directive::OMPD_fuse),
       unsigned(Directive::OMPD_tile),
       unsigned(Directive::OMPD_unroll),
+      unsigned(Directive::OMPD_interchange),
   };
   return loopDirectives;
 }
