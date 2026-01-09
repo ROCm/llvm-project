73d0a7cbc8e8b44494612de64c66d0669a566834
vikhegde <vikram.hegde@amd.com>
2026-01-09 14:12:36 +0530

    Revert "Fixes to be upstreamed - 16"
    This reverts commit 95036fa80e613a9717b33a08d517553fc6dd3d46.

--- llvm/lib/Target/AMDGPU/SILowerControlFlow.cpp
+++ llvm/lib/Target/AMDGPU/SILowerControlFlow.cpp
@@ -742,9 +742,6 @@ bool SILowerControlFlow::removeMBBifRedundant(MachineBasicBlock &MBB) {
   if (PDT)
     PDT->applyUpdates(DTUpdates);
 
-  if (MDT && MDT->getNode(&MBB)) MDT->eraseNode(&MBB);
-  if (PDT && PDT->getNode(&MBB)) PDT->eraseNode(&MBB);
-  
   MBB.clear();
   MBB.eraseFromParent();
   if (FallThrough && !FallThrough->isLayoutSuccessor(Succ)) {
