9cfdcac109b55144dcc62968a00afb53a341332b
Jacob Lambert <jacob.lambert@amd.com>
2026-02-12 15:27:31 -0800

    update lit files to use substitutions

--- amd/comgr/test-lit/lit.cfg.py
+++ amd/comgr/test-lit/lit.cfg.py
@@ -23,3 +23,5 @@ config.environment['AMD_COMGR_CACHE'] = "0"
 config.substitutions.append(('%clang', os.path.join(config.llvm_tools_dir, 'clang')))
 config.substitutions.append(('%llvm-dis', os.path.join(config.llvm_tools_dir, 'llvm-dis')))
 config.substitutions.append(('%llvm-objdump', os.path.join(config.llvm_tools_dir, 'llvm-objdump')))
+config.substitutions.append(('%FileCheck', os.path.join(config.llvm_tools_dir, 'FileCheck')))
+config.substitutions.append(('%amd-llvm-spirv', os.path.join(config.llvm_tools_dir, 'amd-llvm-spirv')))
