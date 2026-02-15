22bba6c1710960db7c1846f5cf2c6e019a2e766c
Ron Lieberman <ron.lieberman@amd.com>
2026-02-15 12:32:52 -0600

    [flang] drop version 23.0.0

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -2351,11 +2351,11 @@ void Driver::PrintHelp(bool ShowHidden) const {
 
 void Driver::PrintVersion(const Compilation &C, raw_ostream &OS) const {
   if (IsFlangMode()) {
-    OS << getClangToolFullVersion("flang") << '\n';
+    OS << getClangToolFullVersion("AMD AFAR drop #23.0.0 02/15/26 flang") << '\n';
   } else {
     // FIXME: The following handlers should use a callback mechanism, we don't
     // know what the client would like to do.
-    OS << getClangFullVersion() << '\n';
+    OS << getClangToolFullVersion("AMD AFAR drop #23.0.0 02/15/26 clang") << '\n';
   }
   const ToolChain &TC = C.getDefaultToolChain();
   OS << "Target: " << TC.getTripleString() << '\n';
