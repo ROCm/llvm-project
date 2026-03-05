9760a5ffbf77ebe30e951d1ed7ba6540b625abae
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2026-03-05 13:37:11 -0800

    [AMDGPU] Add .gfx1250_revision kernel note
    This is to distinguish code for A0 and B0. Revert when we switch to B0.

--- llvm/lib/Target/AMDGPU/AMDGPUHSAMetadataStreamer.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUHSAMetadataStreamer.cpp
@@ -733,6 +733,10 @@ void MetadataStreamerMsgPackV5::emitKernelAttrs(const AMDGPUTargetMachine &TM,
   const Function &Func = MF.getFunction();
   if (Func.getFnAttribute("uniform-work-group-size").getValueAsBool())
     Kern[".uniform_work_group_size"] = Kern.getDocument()->getNode(1);
+
+  const GCNSubtarget &ST = MF.getSubtarget<GCNSubtarget>();
+  if (ST.hasGFX1250Insts())
+    Kern[".gfx1250_revision"] = ST.hasGFX1250A0() ? "A0" : "B0";
 }
 
 //===----------------------------------------------------------------------===//
