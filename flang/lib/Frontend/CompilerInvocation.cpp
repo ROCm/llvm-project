1b562350675bb87844925315ac992c6e84c41b96
Michael Klemm <michael.klemm@amd.com>
2026-03-06 17:25:38 +0100

    [Flang][OpenMP] Switch OpenMP-based AMD allocator & remove Umpire

--- flang/lib/Frontend/CompilerInvocation.cpp
+++ flang/lib/Frontend/CompilerInvocation.cpp
@@ -886,6 +886,11 @@ static bool parseFrontendArgs(FrontendOptions &opts, llvm::opt::ArgList &args,
         (Fortran::common::LanguageFeature::AmdMemoryAllocator));
   }
 
+  // -ffast-amd-memory-allocator
+  if (args.hasArg(clang::options::OPT_ffast_amd_memory_allocator)) {
+    opts.features.Enable(Fortran::common::LanguageFeature::AmdMemoryAllocator);
+  }
+
   if (args.hasArg(clang::options::OPT_falternative_parameter_statement)) {
     opts.features.Enable(Fortran::common::LanguageFeature::OldStyleParameter);
   }
