b4d8d23f63f412d367f34fa0dfdf0f8bda8ff40c
anushe@amd.com <anushe@amd.com@mia1-p01-g31.mia.tensorwave.lan>
2026-03-11 07:41:14 +0000

    openmp/device: restore amdgpu locks from amd-staging

--- openmp/device/src/Synchronization.cpp
+++ openmp/device/src/Synchronization.cpp
@@ -45,7 +45,7 @@ void namedBarrier() {
   // assert(NumThreads % 32 == 0);
 
   uint32_t WarpSize = mapping::getWarpSize();
-  uint32_t NumWaves = NumThreads / WarpSize;
+  uint32_t NumWaves = (NumThreads < WarpSize) ? 1 : NumThreads / WarpSize;
 
   fence::team(atomic::acquire);
 
@@ -91,15 +91,31 @@ void syncThreadsAligned(atomic::OrderingTy Ordering) {
   synchronize::threads(Ordering);
 }
 
-// TODO: Don't have wavefront lane locks. Possibly can't have them.
-void unsetLock(omp_lock_t *) { __builtin_trap(); }
-int testLock(omp_lock_t *) { __builtin_trap(); }
-void initLock(omp_lock_t *) { __builtin_trap(); }
-void destroyLock(omp_lock_t *) { __builtin_trap(); }
-void setLock(omp_lock_t *) { __builtin_trap(); }
-
 constexpr uint32_t UNSET = 0;
 constexpr uint32_t SET = 1;
+
+void unsetLock(omp_lock_t *Lock) {
+  (void)atomicExchange((uint32_t *)Lock, UNSET, atomic::seq_cst);
+}
+
+int testLock(omp_lock_t *Lock) {
+  return atomic::add((uint32_t *)Lock, 0u, atomic::seq_cst);
+}
+
+void initLock(omp_lock_t *Lock) { unsetLock(Lock); }
+
+void destroyLock(omp_lock_t *Lock) { unsetLock(Lock); }
+
+void setLock(omp_lock_t *Lock) {
+  uint64_t lowestActiveThread = utils::ffs(mapping::activemask()) - 1;
+  if (mapping::getThreadIdInWarp() == lowestActiveThread) {
+    while (!atomic::cas((uint32_t *)Lock, UNSET, SET, atomic::seq_cst,
+                        atomic::seq_cst, atomic::MemScopeTy::system)) {
+      __builtin_amdgcn_s_sleep(0);
+    }
+  }
+  // test_lock will now return true for any thread in the warp
+}
 
 void unsetCriticalLock(omp_lock_t *Lock) {
   [[maybe_unused]] uint32_t before =
@@ -278,6 +294,12 @@ void __kmpc_end_single(IdentTy *Loc, int32_t TId) {
 }
 
 void __kmpc_flush(IdentTy *Loc) { fence::kernel(atomic::seq_cst); }
+
+void __kmpc_flush_acquire(IdentTy *Loc) { fence::kernel(atomic::acquire); }
+
+void __kmpc_flush_release(IdentTy *Loc) { fence::kernel(atomic::release); }
+
+void __kmpc_flush_acqrel(IdentTy *Loc) { fence::kernel(atomic::acq_rel); }
 
 uint64_t __kmpc_warp_active_thread_mask(void) { return mapping::activemask(); }
 
