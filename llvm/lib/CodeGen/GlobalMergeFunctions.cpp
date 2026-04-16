8e8992d6aaac9ea413a9ad353152eb904bc1019a
vikhegde <vikram.hegde@amd.com>
2026-04-16 18:58:08 +0530

    Port immutableModuleSummaryAnalysis to NPM

--- llvm/lib/CodeGen/GlobalMergeFunctions.cpp
+++ llvm/lib/CodeGen/GlobalMergeFunctions.cpp
@@ -617,6 +617,8 @@ bool GlobalMergeFuncPassWrapper::runOnModule(Module &M) {
 
 PreservedAnalyses GlobalMergeFuncPass::run(Module &M,
                                            AnalysisManager<Module> &AM) {
-  bool Changed = GlobalMergeFunc(ImportSummary).run(M);
+  const ModuleSummaryIndex *Index =
+      AM.getResult<ImmutableModuleSummaryIndexAnalysis>(M).getIndex();
+  bool Changed = GlobalMergeFunc(Index).run(M);
   return Changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
 }
