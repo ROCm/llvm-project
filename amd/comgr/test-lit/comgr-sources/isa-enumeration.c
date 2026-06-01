0b8d8ed48f48e80426ba6e3ebe9d8013f032dab2
Soumil Kushwaha <Soumil.Kushwaha@amd.com>
2026-06-01 10:45:21 -0500

    [Comgr] Port get_data_isa_name_test from CTest to LIT
    Ports get_data_isa_name_test to LIT and splits into two separate tests, isa-enumeration and get-data-isa-name.

---
+++ amd/comgr/test-lit/comgr-sources/isa-enumeration.c
@@ -0,0 +1,26 @@
+//===- isa-enumeration.c -------------------------------------------------===//
+//
+// Part of Comgr, under the Apache License v2.0 with LLVM Exceptions. See
+// amd/comgr/LICENSE.TXT in this repository for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "amd_comgr.h"
+#include "common.h"
+
+#define MAX_ISA_NAME_SIZE 1024
+
+int main(int argc, char *argv[]) {
+  size_t IsaCount;
+  amd_comgr_(get_isa_count(&IsaCount));
+  if (IsaCount <= 0)
+    fail("ISA Count: %d\n", IsaCount);
+  for (int i = 0; i < IsaCount; i++) {
+    const char *Name;
+    char IsaName[MAX_ISA_NAME_SIZE];
+    amd_comgr_(get_isa_name(i, &Name));
+    strncpy(IsaName, Name, MAX_ISA_NAME_SIZE);
+    printf("%s\n", IsaName);
+  }
+}
