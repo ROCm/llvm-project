48841c83f95b215538a2c9618775d7f32e19bf97
Soumil Kushwaha <Soumil.Kushwaha@amd.com>
2026-06-02 17:47:13 -0500

    Merged isa-enumeration LIT test into get-data-isa-name.c LIT test and added feature enumeration to isa-enumeration source. Now, running get-data-isa-name will iterate isa-enumeration over all features and then get-data-isa-name is called on each ISA and their respective valid features.

--- amd/comgr/test-lit/get-data-isa-name.c
+++ amd/comgr/test-lit/get-data-isa-name.c
@@ -1,16 +1,10 @@
 // COM: Test Comgr get_data_isa_name() API
-// RUN: %clang -target amdgcn-amd-amdhsa -mcpu=gfx900 -nogpulib -nogpuinc \
-// RUN:   -c %S/comgr-sources/shared.cl -o %t.gfx900.o
-// RUN: get-data-isa-name %t.gfx900.o "amdgcn-amd-amdhsa--gfx900"
-
-// RUN: %clang -target amdgcn-amd-amdhsa -mcpu=gfx908 -nogpulib -nogpuinc \
-// RUN:   -c %S/comgr-sources/shared.cl -o %t.gfx908.o
-// RUN: get-data-isa-name %t.gfx908.o "amdgcn-amd-amdhsa--gfx908"
-
-// RUN: %clang -target amdgcn-amd-amdhsa -mcpu=gfx1030 -nogpulib -nogpuinc \
-// RUN:   -c %S/comgr-sources/shared.cl -o %t.gfx1030.o
-// RUN: get-data-isa-name %t.gfx1030.o "amdgcn-amd-amdhsa--gfx1030"
-
-// RUN: %clang -target amdgcn-amd-amdhsa -mcpu=gfx9-generic -mcode-object-version=6 -nogpulib -nogpuinc \
-// RUN:   -c %S/comgr-sources/shared.cl -o %t.gfx9-generic.o
-// RUN: get-data-isa-name %t.gfx9-generic.o "amdgcn-amd-amdhsa--gfx9-generic"
+// RUN: mapfile -t isa_arr < <(./isa-enumeration)
+// RUN: for isa in ${isa_arr[@]}; do \
+// RUN: gpu=${isa##*--}; \
+// RUN: %clang -target amdgcn-amd-amdhsa -mcpu=$gpu -nogpulib -nogpuinc \
+// RUN:   -c %S/comgr-sources/gtgra.cl -o %t.o; \
+// RUN: %clang -target amdgcn-amd-amdhsa -mcpu=$gpu -nogpulib -nogpuinc \
+// RUN:   -shared %S/comgr-sources/gtgra.cl -o %t.so; \
+// RUN: get-data-isa-name %t.o %t.so $isa; \
+// RUN: done
