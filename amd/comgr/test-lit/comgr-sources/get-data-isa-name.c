0044f20fe808b366242239dd2839b8a96d2db05f
Soumil Kushwaha <Soumil.Kushwaha@amd.com>
2026-06-04 12:42:26 -0500

    changed to use SizeIsa variable used for get-data-isa-name test

--- amd/comgr/test-lit/comgr-sources/get-data-isa-name.c
+++ amd/comgr/test-lit/comgr-sources/get-data-isa-name.c
@@ -12,12 +12,13 @@
 int main(int argc, char *argv[]) {
   char *BufReloc, *BufExec;
   size_t SizeReloc, SizeExec;
+  size_t SizeIsa = MAX_ISA_NAME_SIZE;
   char IsaName[MAX_ISA_NAME_SIZE];
   amd_comgr_data_t DataReloc, DataExec;
 
   if (argc != 4)
-    fail("Usage: get-data-isa-name <code-object-path> <code-shared-object-path> <expected-isa-name>");
-  
+    fail("Usage: get-data-isa-name <code-object-path> <code-shared-object-path <expected-isa-name>");
+
   SizeReloc = setBuf(argv[1], &BufReloc);
   SizeExec = setBuf(argv[2], &BufExec);
 
@@ -25,10 +26,10 @@ int main(int argc, char *argv[]) {
   amd_comgr_(create_data(AMD_COMGR_DATA_KIND_EXECUTABLE, &DataExec));
   amd_comgr_(set_data(DataReloc, SizeReloc, BufReloc));
   amd_comgr_(set_data(DataExec, SizeExec, BufExec));
-  amd_comgr_(get_data_isa_name(DataReloc, &SizeReloc, IsaName));
+  amd_comgr_(get_data_isa_name(DataReloc, &SizeIsa, IsaName));
   if (strcmp(IsaName, argv[3]))
     fail("incorrect isa name: expected %s, saw %s", argv[3], IsaName);
-  amd_comgr_(get_data_isa_name(DataExec, &SizeExec, IsaName));
+  amd_comgr_(get_data_isa_name(DataExec, &SizeIsa, IsaName));
   if (strcmp(IsaName, argv[3]))
     fail("incorrect isa name: expected %s, saw %s", argv[3], IsaName);
   amd_comgr_(release_data(DataReloc));
