b78a1903b42693416363454077722b5a2df55004
Soumil Kushwaha <Soumil.Kushwaha@amd.com>
2026-06-18 13:41:06 -0500

    moved enumeration to a python script as per Scott's advice, removes enumeration dependency on linux bash commands

--- amd/comgr/test-lit/lit.cfg.py
+++ amd/comgr/test-lit/lit.cfg.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import sys
 
 import lit.formats
 
@@ -39,3 +40,6 @@ config.substitutions.append(('%llvm-objdump', _fwd(config.llvm_tools_dir, 'llvm-
 config.substitutions.append(('%llvm-readelf', _fwd(config.llvm_tools_dir, 'llvm-readelf')))
 config.substitutions.append(('%FileCheck', _fwd(config.llvm_tools_dir, 'FileCheck')))
 config.substitutions.append(('%amd-llvm-spirv', _fwd(config.llvm_tools_dir, 'amd-llvm-spirv')))
+
+# Interpreter used to run Python test helpers (e.g. enumerate-isa-check.py).
+config.substitutions.append(('%python', _fwd(sys.executable)))
