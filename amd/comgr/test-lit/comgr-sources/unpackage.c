09aa022d16b52f3e87d8606bf3ffdaa77339156a
alessandra simmons <alessandra@adrs.pub>
2026-06-19 13:53:34 -0400

    test failure cases

--- amd/comgr/test-lit/comgr-sources/unpackage.c
+++ amd/comgr/test-lit/comgr-sources/unpackage.c
@@ -9,17 +9,33 @@
 #include "amd_comgr.h"
 #include "common.h"
 
+static amd_comgr_data_kind_t parseDataKind(const char *Kind) {
+  if (strcmp(Kind, "package") == 0)
+    return AMD_COMGR_DATA_KIND_PACKAGE;
+  if (strcmp(Kind, "bc") == 0)
+    return AMD_COMGR_DATA_KIND_BC;
+  printf("Error: unknown data kind '%s'\n", Kind);
+  exit(-1);
+}
+
 int main(int argc, char *argv[]) {
   char *PackageData;
   size_t PackageSize;
 
   const char *PackageName = "package.bc";
 
+  amd_comgr_data_kind_t InputKind = AMD_COMGR_DATA_KIND_PACKAGE;
+  int ExpectFail = 0;
+
   int ArgIdx = 1;
   while (ArgIdx < argc && argv[ArgIdx][0] == '-') {
     const char *Arg = argv[ArgIdx];
     if (strncmp(Arg, "--name=", 7) == 0) {
       PackageName = Arg + 7;
+    } else if (strncmp(Arg, "--data-kind=", 12) == 0) {
+      InputKind = parseDataKind(Arg + 12);
+    } else if (strcmp(Arg, "--expect-fail") == 0) {
+      ExpectFail = 1;
     } else {
       printf("Error: unknown flag '%s'\n", Arg);
       return -1;
@@ -45,7 +61,7 @@ int main(int argc, char *argv[]) {
   PackageSize = setBuf(PackagePath, &PackageData);
 
   amd_comgr_(create_data_set(&InputPackages));
-  amd_comgr_(create_data(AMD_COMGR_DATA_KIND_PACKAGE, &OnePackage));
+  amd_comgr_(create_data(InputKind, &OnePackage));
   amd_comgr_(set_data(OnePackage, PackageSize, PackageData));
   amd_comgr_(set_data_name(OnePackage, PackageName));
   amd_comgr_(data_set_add(InputPackages, OnePackage));
@@ -58,6 +74,18 @@ int main(int argc, char *argv[]) {
 
   amd_comgr_target_id_t PackageEntryIDs[] = {{Triple, Arch}};
   amd_comgr_(action_info_set_package_entry_ids(DataAction, PackageEntryIDs, 1));
+
+  if (ExpectFail) {
+    fail_amd_comgr_(do_action(AMD_COMGR_ACTION_UNPACKAGE, DataAction,
+                              InputPackages, OutputBitcode));
+    amd_comgr_(release_data(OnePackage));
+    amd_comgr_(destroy_action_info(DataAction));
+    amd_comgr_(destroy_data_set(OutputBitcode));
+    amd_comgr_(destroy_data_set(InputPackages));
+    free(PackageData);
+    return 0;
+  }
+
   amd_comgr_(do_action(AMD_COMGR_ACTION_UNPACKAGE, DataAction, InputPackages,
                        OutputBitcode));
 
@@ -93,5 +121,4 @@ int main(int argc, char *argv[]) {
   free(PackageData);
 
   return 0;
-
 }
