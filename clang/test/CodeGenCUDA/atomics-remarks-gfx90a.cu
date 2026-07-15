8ad068b907da117885fc2521c3ee3545df1dab7a
Teja Alaghari <teja.alaghari@amd.com>
2026-07-15 20:14:16 +0530

    [WaveTransform] Update the FileCheck pattern for clang test (#3395)
    Enable LWT and update the FileCheck pattern for below clang test:
    - clang/test/CodeGenCUDA/atomics-remarks-gfx90a.cu

--- clang/test/CodeGenCUDA/atomics-remarks-gfx90a.cu
+++ clang/test/CodeGenCUDA/atomics-remarks-gfx90a.cu
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 %s -triple=amdgcn-amd-amdhsa -fcuda-is-device \
+// RUN:   -mllvm -amdgpu-late-wave-transform=1 \
 // RUN:   -target-cpu gfx90a -Rpass=atomic-expand -S -o - 2>&1 | \
 // RUN:   FileCheck %s --check-prefix=GFX90A-CAS
 
@@ -10,7 +11,7 @@
 // GFX90A-CAS: A compare and swap loop was generated for an atomic fadd operation at system memory scope
 // GFX90A-CAS-LABEL: _Z14atomic_add_casPf
 // GFX90A-CAS:  flat_atomic_cmpswap
-// GFX90A-CAS:  s_cbranch_execnz
+// GFX90A-CAS:  s_cbranch_execz
 __device__ float atomic_add_cas(float *p) {
   return __atomic_fetch_add(p, 1.0f, memory_order_relaxed);
 }
