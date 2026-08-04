b210213ae602fe8116122a89f87b50aea852abab
Sergio Afonso <Sergio.AfonsoFumero@amd.com>
2026-08-04 16:59:21 +0100

    [MLIR][OpenMP] Support calls added between MarkDeclareTarget runs
    Currently, if there are multiple executions of the `MarkDeclareTarget`
    pass in a compiler pipeline and somewhere between both runs function
    calls get added to a non-declare_target function that was marked as such
    implicitly, potential changes to the `device_type` won't get propagated.
    
    This is because we can't distinguish between a user-specified
    `declare_target` function attribute and one added by that pass. This
    patch addresses this by adding a new parameter to `DeclareTargetAttr`
    that is used by that pass to know whether new `declare_target`
    information could be propagated to it.
    
    The `automap` and `implicit` parameters are given default values to
    simplify the representation of these attributes.

--- mlir/lib/Dialect/OpenMP/Transforms/FunctionFiltering.cpp
+++ mlir/lib/Dialect/OpenMP/Transforms/FunctionFiltering.cpp
@@ -58,11 +58,8 @@ class FunctionFilteringPass
       if (declareTargetOp && declareTargetOp.isDeclareTarget())
         declareType = declareTargetOp.getDeclareTargetDeviceType();
 
-      // Filtering a function here means deleting it if it doesn't contain a
-      // target region. Else we explicitly set the omp.declare_target
-      // attribute. The second stage of function filtering at the MLIR to LLVM
-      // IR translation level will remove functions that contain the target
-      // region from the generated LLVM IR.
+      // Only filter host functions from device modules because the host needs
+      // to provide fallback implementations of device code.
       if (declareType != omp::DeclareTargetDeviceType::host)
         return WalkResult::advance();
 
