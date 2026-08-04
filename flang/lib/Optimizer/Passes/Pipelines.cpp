6fd43368ee20bdc4df1610a628e45ee786834c60
Sergio Afonso <Sergio.AfonsoFumero@amd.com>
2026-08-04 11:04:00 +0100

    [Flang][OpenMP] Remove the DeleteUnreachableTargets pass
    The `DeleteUnreachableTargets` pass was initially added to work around
    a problem caused by the interaction between the function filtering and
    host op filtering passes and generic MLIR optimizations.
    
    Specifically, by running function and host op filtering before these
    optimizations, we would lose the ability to detect unreachable
    `omp.target` operations when compiling for the device. This would cause
    GPU kernels being created for them, for which no host counterpart
    existed.
    
    By now having moved both passes towards the end of the compilation
    pipeline, after FIR to LLVM lowering, removal of unreachable host and
    device code is no longer impacted by them. This makes the
    `DeleteUnreachableTargets` pass redundant.

--- flang/lib/Optimizer/Passes/Pipelines.cpp
+++ flang/lib/Optimizer/Passes/Pipelines.cpp
@@ -372,9 +372,6 @@ void createOpenMPFIRPassPipeline(mlir::PassManager &pm,
   pm.addPass(flangomp::createAutomapToTargetDataPass());
   pm.addPass(flangomp::createMapInfoFinalizationPass(opts.deferDescMap));
 
-  // Delete unreachable target operations before FunctionFilteringPass
-  // extracts them.
-  pm.addPass(flangomp::createDeleteUnreachableTargetsPass());
   pm.addPass(flangomp::createGenericLoopConversionPass());
   if (opts.isTargetDevice && opts.enableOffloadGlobalFiltering)
     pm.addPass(flangomp::createGlobalFilteringPass());
