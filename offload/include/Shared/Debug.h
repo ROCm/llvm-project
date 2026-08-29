78adb7aae4ac92847cb6e9806ad6a02be779ca3f
Larry Meadows <lmeadows@amd.com>
2026-08-29 15:41:05 -0700

    [offload] Re-enable InfoIdToODT static_assert, dropping #if FIXME guard

--- offload/include/Shared/Debug.h
+++ offload/include/Shared/Debug.h
@@ -698,9 +698,7 @@ template <uint32_t InfoId> static constexpr const char *InfoIdToODT() {
   };
 
   constexpr const char *result = getId();
-#if FIXME
   static_assert(result != nullptr, "Unknown InfoId being used");
-#endif
   return result;
 }
 
