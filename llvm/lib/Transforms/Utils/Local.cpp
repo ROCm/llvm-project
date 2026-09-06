98ddec9a539c06b2e5adddaae233cc212c20ffd4
Ron Lieberman <ron.lieberman@amd.com>
2026-09-06 14:12:29 -0500

    Revert "Re-apply "[PromoteMemToReg] Insert store undef when removing lifetime markers" (#218935)"
    This reverts commit 44ea65c07a600ca1c32912b5cd7356f6792b2b03.

--- llvm/lib/Transforms/Utils/Local.cpp
+++ llvm/lib/Transforms/Utils/Local.cpp
@@ -1694,9 +1694,6 @@ void llvm::ConvertDebugDeclareToDebugValue(DbgVariableRecord *DVR,
   auto *DIExpr = DVR->getExpression();
   Value *DV = SI->getValueOperand();
 
-  if (isa<UndefValue>(DV) && !isa<PoisonValue>(DV))
-    return;
-
   DebugLoc NewLoc = getDebugValueLoc(DVR);
 
   DIExpr = tryRemoveNewDIExpressionIndirection(DIExpr, DV->getType());
