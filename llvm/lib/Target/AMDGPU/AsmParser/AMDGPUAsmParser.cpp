d28e28bcb5e2951def293398048488d70cee3188
Ron Lieberman <ron.lieberman@amd.com>
2026-09-09 17:29:06 -0500

    Revert "AMDGPU: Remove xnack and sramecc subtarget features (#212792)"
    breaks comgr
    
    This reverts commit a86b58566a2581160938db41fa258ff84457b80c.

--- llvm/lib/Target/AMDGPU/AsmParser/AMDGPUAsmParser.cpp
+++ llvm/lib/Target/AMDGPU/AsmParser/AMDGPUAsmParser.cpp
@@ -6390,7 +6390,8 @@ bool AMDGPUAsmParser::ParseDirectiveAMDHSAKernel() {
         return Error(IDRange.Start, "directive requires gfx8+", IDRange);
       if (!isUInt<1>(Val))
         return OutOfRangeError(ValRange);
-      bool XnackOn = getTargetStreamer().getTargetID()->isXnackOnOrAny();
+      bool XnackOn = getTargetStreamer().getTargetID()->isXnackOnOrAny() ||
+                     getSTI().hasFeature(AMDGPU::FeatureXNACK);
       if (Val != XnackOn) {
         return getParser().Error(
             IDRange.Start,
